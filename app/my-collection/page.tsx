1091ce166d71636670588ade6d6d046ecc2ada1e
Ayaan Adil <ayaanninja2403@gmail.com>
2025-01-09 01:24:17 +0530

    Update page.tsx

--- app/my-collection/page.tsx
+++ app/my-collection/page.tsx
@@ -21,7 +21,7 @@ export default function MyCollection() {
       <BackgroundCollage />
       <h1 className="text-4xl sm:text-5xl font-bold mb-4 pixel-font mt-36 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">My NFT Collection</h1>
       {isLoading ? (
-        <p className="text-center pixel-font">Loading your NFTs...</p>
+        <p className="text-center mt-10 pixel-font">Loading your NFTs...</p>
       ) : (
         <p className="text-center pixel-font">You haven't created any NFTs yet.</p>
       )}
